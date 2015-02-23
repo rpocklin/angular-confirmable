f03965700edbc43a76e860475161e0fb870c59ad
Robert Pocklington <rpocklin@gmail.com>
2015-02-24 08:32:24 +1100

    Updated karma config.

--- karma.conf.js
+++ karma.conf.js
@@ -20,8 +20,7 @@ module.exports = function(config) {
       'bower_components/jasmine-sinon/lib/jasmine-sinon.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
-      'src/**/*.html',
-      'src/**/*.js'
+      'src/angular-confirmable.js'
     ],
 
     exclude:   [],
